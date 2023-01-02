96646d3f07f78381980ef697e58a2ae7433e197e
Justin Nelson <justin13@berkeley.edu>
2023-01-02 02:43:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, } from 'react';
+import Toptext from "./Toptext"
+import InputText from './InputText';
+import './App.css'
+
 
 function App() {
+  // eslint-disable-next-line 
+  const [input, setInput] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <body>
+    <Toptext/ 
+    >
+    
+    </body>
+    <InputText input = {input}/>
+    <input type = "text" 
+    
+    />
+
+    </>
+  
+  )
 }
 
 export default App;
---
+++ src/InputText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function InputText({ input }) {
+  return (
+    <div>
+        <a href="https://open.spotify.com/playlist/2qsIO658DnMLlc9GeKsayA?si=84de7f13a6af49e6"> EPIC PLAYLIST!!!</a>            
+        <h1>
+        And some sweets for my lovely!
+        <img src="./ImageDetails.PNG" alt="Italian Trulli"/>
+          </h1>
+          mysterious textbox?
+    </div>
+  )
+}

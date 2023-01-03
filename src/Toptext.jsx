3e8dc4bcdfb4c581b7659fcfd7f93d9ce4eb339c
Justin Nelson <justin13@berkeley.edu>
2023-01-02 16:37:42 -0800

    Fixed Typos in TopText

--- src/Toptext.jsx
+++ src/Toptext.jsx
@@ -6,8 +6,8 @@ export default function toptext() {
     <p>
       Hello Girlfriend, 
       Happy Six Months. I hope you don't look at this website and think about what a perfect metaphor it is for our relationship- better in the first half with serious corners
-      being cut in the second. I kid. Obviously. Every day with you has been even better than the last and reenforces how lucky I am to have you. I hope the metaphor you see here is in 
-      the raw, HTML unabated by fancy CSS, and is true to its form is just like me. I am running out of room and because I have forgotten how to resize text I must make these final words count. 
+      being cut in the second. I kid. Obviously. Every day with you has been even better than the last and reinforces how lucky I am to have you. I hope the metaphor you see here is in 
+      the raw HTML, unabated by fancy CSS, and is true to its form- just like me. I am running out of room and because I have forgotten how to resize text I must make these final words count. 
       My heart skips a beat everytime I see you. You are undoubtedly the most kind, insightful and beautiful person I have ever had the great pleasre of meeting. Please know that I 
       am thinking of your warmly from my side of the planet and that I look forward to seeing you again. git 
       :) 
